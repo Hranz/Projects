8c30c9889010a9af1ffe68728dddbb1af3be5feb
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-01-18 19:36:02 -0800

    Create TreeException.java

---
+++ BST/TreeException.java
@@ -0,0 +1,12 @@
+/* 
+   Programmers: Kristoffer Larson
+   
+   Description:
+      It handles runtime exceptions in the BST.
+
+*/
+public class TreeException extends RuntimeException {
+   public TreeException(String s) {
+      super(s);
+   } // end constructor
+} // end TreeException
