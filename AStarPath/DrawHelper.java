212710b6f8cbc096540741546fdb8c4277daff19
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-03-16 21:54:08 -0700

    Create DrawHelper.java

---
+++ AStarPath/DrawHelper.java
@@ -0,0 +1,11 @@
+import SimulationFramework.*;
+
+public class DrawHelper {
+   
+   private AnimatePanel animatePanel;
+   
+   public void initializePanel(AnimatePanel a) {
+      animatePanel = a;
+   } 
+
+}
