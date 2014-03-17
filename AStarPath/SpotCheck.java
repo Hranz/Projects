d8066b16bdf24e4173f2e781dcda3ce724182cfb
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-03-16 21:54:44 -0700

    Create SpotCheck.java

---
+++ AStarPath/SpotCheck.java
@@ -0,0 +1,12 @@
+/*
+   Programmer: Kristoffer Larson
+   Date: March , 2014
+   
+   Description: Checks the WayPoint for the PlayerBot
+*/
+
+import SimulationFramework.*;
+
+public class SpotCheck extends PlayerBot {
+
+}
