d8b48ea09cd92a97cb71c6cfc46b52edc6325ca2
Kristoffer Larson <larson.kristoffert@gmail.com>
2014-03-25 22:56:28 -0700

    Update PlayerBot.java

--- AStarPath/PlayerBot.java
+++ AStarPath/PlayerBot.java
@@ -12,36 +12,51 @@
 import SimulationFramework.Bot;
 import SimulationFramework.Marker;
 import SimulationFramework.AnimatePanel;
+import SimulationFramework.*;
 import java.awt.Point;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Stack;
 import java.util.List;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Iterator;
 
 public class PlayerBot extends Bot {
    
-   private int lastX, lastY, lastHeight, strength, wealth;
+   private int lastX, lastY, lastHeight, str, w;
+   private Point dest;
+   private boolean hasTMap;
    private HashMap<Point, WayPoint> map;
    private WayPoint go;
+   private AnimatePanel animatePanel;
+   private SimFrame sf;
    
-   public PlayerBot(String label, Point a, int height, int str, int w) {
+   public PlayerBot(String label, Point a, int height, int str, int w, Point dest,
+      AnimatePanel animatePanel) {
+      
       super(label, a, Color.RED);
-      strength = str;
-      wealth = w;
+      this.str = str;
+      this.w = w;
       lastX = (int)a.getX();
-		lastY = (int)a.getY();
+      lastY = (int)a.getY();
       lastHeight = height;
+      hasTMap = false;
+      this.dest = dest;
+      this.animatePanel = animatePanel;
    }//End PlayerBot() Constructor
    
-   public PlayerBot(String label, Point a, int height) {
+   public PlayerBot(String label, Point a, int height, Point dest, AnimatePanel animatePanel) {
       super(label, a, Color.RED);
-      strength = 2000;
-      wealth = 1000;
+      str = 2000;
+      w = 1000;
       lastX = (int)a.getX();
-		lastY = (int)a.getY();
+      lastY = (int)a.getY();
       lastHeight = height;
+      hasTMap = false;
+      this.dest = dest;
+      this.animatePanel = animatePanel;
    }//End PlayerBot() Constructor
    
    public void gainMap(HashMap<Point, WayPoint> map) {
@@ -49,29 +64,45 @@ public class PlayerBot extends Bot {
    }//End gainMap() method
    
    public int getWealth() {
-      return wealth;
+      return w;
    }//End getWealth() method
    
    public int getStrength() {
-      return strength;
+      return str;
    }//End getStrength() method
    
-   public void setWealth(int wealth) {
-      this.wealth = wealth;
+   public boolean getTMap() {
+      return hasTMap;
+   }//End getTMap() method
+   
+   public Point getDest() {
+      return dest;
+   }//End getDest() method
+   
+   public void setWealth(int w) {
+      this.w = w;
    }//End setWealth() method
    
-   public void setStrength(int strength) {
-      this.strength = strength;
+   public void setStrength(int str) {
+      this.str = str;
    }//End setStrength() method
    
-   public void checkNode(WayPoint aWayPoint) {
+   public void setTMap(boolean has) {
+      hasTMap = has;
+   }//End getTMap() method
    
-   }//End checkNode() method
+   public void setDest(Point dest) {
+      this.dest = dest;
+   }
+   
+   public void gainFrame(SimFrame s) {
+      sf = s;
+   }
    
    public void reset() {
-		super.reset();
-		lastX = (int) point.getX(); 
-		lastY = (int) point.getY();
+      super.reset();
+      lastX = (int) point.getX(); 
+      lastY = (int) point.getY();
    }//End reset() method
    
    public void setMove(WayPoint go) {
@@ -84,50 +115,78 @@ public class PlayerBot extends Bot {
       newX = go.getWX();
       newY = go.getWY();
       height = go.getHeight();
-
+   
       int distanceOfMove = (int)(Math.sqrt(Math.pow(newX - lastX, 2)
          + Math.pow(newY - lastY, 2) + Math.pow(lastHeight - height, 2)));
-		
+   	
       moveTo(newX, newY);
       //Reduces the strength of the "bot"
-      strength -= distanceOfMove;
+      str -= distanceOfMove;
       //Sets current location of the "bot"
-		lastX = newX;
-	   lastY = newY;
+      lastX = newX;
+      lastY = newY;
       lastHeight = height;
    }
    
    //Handles the simple movement between WayPoints
-//    public void simpleMove() {
-//       if (!moves.isEmpty()) {
-//             //Find closest WayPoint to destination
-//             oldDist = 200000.0;
-//             for (int i = 0; i < moves.size(); i++) {
-//                aWayPoint = moves.get(i);
-//                dist = Math.sqrt(Math.pow(((int)dest.getY() - aWayPoint.getWY()), 2) 
-//                   + Math.pow(((int)dest.getX() - aWayPoint.getWX()), 2) 
-//                   + Math.pow(destH - aWayPoint.getHeight(), 2));
-//                   
-//                if (dist < oldDist) {
-//                   oldDist = dist;
-//                   temp = new Point(aWayPoint.getWX(), aWayPoint.getWY());
-//                }//End if
-//             }//End for
-//             
-//             if (temp != null) {
-//                aBot.setMove(map.get(temp));
-//                aBot.move();//Bot moves and reduces strength
-//             }//End if
-//    }//End simpleMove() method
+   public void simpleMove() {
+   //       if (!moves.isEmpty()) {
+   //             //Find closest WayPoint to destination
+   //             oldDist = 200000.0;
+   //             for (int i = 0; i < moves.size(); i++) {
+   //                aWayPoint = moves.get(i);
+   //                dist = Math.sqrt(Math.pow(((int)dest.getY() - aWayPoint.getWY()), 2) 
+   //                   + Math.pow(((int)dest.getX() - aWayPoint.getWX()), 2) 
+   //                   + Math.pow(destH - aWayPoint.getHeight(), 2));
+   //                   
+   //                if (dist < oldDist) {
+   //                   oldDist = dist;
+   //                   temp = new Point(aWayPoint.getWX(), aWayPoint.getWY());
+   //                }//End if
+   //             }//End for
+   //             
+   //             if (temp != null) {
+   //                aBot.setMove(map.get(temp));
+   //                aBot.move();//Bot moves and reduces strength
+   //             }//End if
+   //       }//End if
+   }//End simpleMove() method
+   
+   private void waitT() {
+      try {
+         Thread.sleep(10);
+      } 
+      catch(InterruptedException ex) {
+         Thread.currentThread().interrupt();
+      }
+   }//End wait() method
+   
+   private boolean setContains(PriorityQueue<Node> openSet, HashSet<Node> closedSet, Node in) {
+      Iterator<Node> iterator = openSet.iterator();
+      while (iterator.hasNext()) {
+         if (iterator.next().getPoint().equals(in.getPoint())) {
+            return true;
+         }
+      }
+      
+      iterator = closedSet.iterator();
+      while (iterator.hasNext()) {
+         if (iterator.next().getPoint().equals(in.getPoint())) {
+            return true;
+         }
+      }
+      return false;
+      
+   }//End setContains() method
    
    //Handles A* algorithm
-   public List<WayPoint> aStarMove(Point dest, List<WayPoint> list) {
+   public List<WayPoint> aStarMove(Point dest, List<WayPoint> list, LarsonRuizTreasurePath app) {
       //Make into a class?
       PriorityQueue<Node> openSet = new PriorityQueue(20, new NodeComparator());
-      ArrayList<Node> closedSet = new ArrayList<Node>();
+      HashSet<Node> closedSet = new HashSet<Node>();
       ArrayList<Point> tempNeigh;
       Node in;
-      boolean goal = false;
+      boolean goal = false, contains = false;
       
       //First Node in openSet
       Node node = new Node(this.getPoint(), 0, this.getPoint().distance(dest), null);
@@ -140,9 +199,12 @@ public class PlayerBot extends Bot {
          if (node.getPoint().equals(dest)) {
             goal = true;
             closedSet.add(node);
+            animatePanel.addTemporaryDrawable(new Marker(node.getPoint(), Color.BLACK, 2));
+            animatePanel.repaint();
             openSet.clear();
             //break;
-         } else {
+         } 
+         else {
             double dist;
             tempNeigh = aWayPoint.getNeigh(); //ArrayList of points
             if (tempNeigh.size() > 0) { 
@@ -150,18 +212,35 @@ public class PlayerBot extends Bot {
                   in = new Node(tempNeigh.get(i), node.getDist() +
                      node.getPoint().distance(tempNeigh.get(i)), 
                      tempNeigh.get(i).distance(dest), node);
-                  if (!(openSet.contains(in) || closedSet.contains(in)) && in != null) {
+                  
+                  contains = setContains(openSet, closedSet, in);
+                  
+                  //Can't use contains method for PriorityQueue and HashSet. Why?
+                  if (!contains) {
                      openSet.add(in);
-                     in.makeWhite();
-                     //AnimatePanel.addTemporaryDrawable(new Marker(tempNeigh.get(i), Color.WHITE, 3));
-                     //AnimatePanel.repaint();
+                     animatePanel.addTemporaryDrawable(new Marker(tempNeigh.get(i), Color.WHITE, 3));
+                     //animatePanel.repaint();
+                     //waitT();
+                     sf.checkStateToWait();
                   }
                }//End for
-               openSet.remove(node);
-               closedSet.add(node);
-               node = openSet.peek();
+            }//End if
+            else { 
+               return null; }
+            
+            openSet.remove(node);
+            closedSet.add(node);
+            animatePanel.addTemporaryDrawable(new Marker(node.getPoint(), Color.BLACK, 2));
+            //animatePanel.repaint();
+            //waitT();
+            sf.checkStateToWait();
+               
+            node = openSet.peek();
+            if (node != null)
                aWayPoint = map.get(node.getPoint());
-            }
+            else 
+               return null;
+            
          }//End if
       }//End while
       
@@ -177,14 +256,14 @@ public class PlayerBot extends Bot {
             node = node.getPrev();
             //aWayPoint = map.get(node.getPoint());
          }//End while
-
+      
             
          while(stack.size() > 0){
-             list.add(map.get(stack.pop().getPoint()));
+            list.add(map.get(stack.pop().getPoint()));
          }
          list.remove(0);//Remove the initial node
          return list;
-
+      
       
       }//End if
       
